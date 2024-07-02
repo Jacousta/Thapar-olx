6840580740473a458bea420ab757dfd3769943ea
Jacousta <asharma22_be21@thapar.edu>
2024-07-02 16:45:01 +0530

    devbugdorted

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'client',
   plugins: [react()],
   optimizeDeps: {
-    include: ['js-cookie'],
-  },
-  publicDir: 'client/public',
-  build: {
-    outDir: '../dist',
-    rollupOptions: {
-      input: 'client/index.html',
-    },
-  },
+    include: ['js-cookie'],},
 })
